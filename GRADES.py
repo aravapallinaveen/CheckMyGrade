5c77955d5646c3ede8f465c8776a05e15e1b149f
Naveen Aravapalli <naveengupta.nav.a@gmail.com>
2025-02-27 12:51:15 -0800

    full classes

--- GRADES.py
+++ GRADES.py
@@ -78,6 +78,19 @@ class Grade:
         print(f"Grade: {self.grade}")
         print(f"Marks Range: {self.marks_range}")
 
+    def add_grade(self, grade, marks_range):
+        self.grade = grade
+        self.marks_range = marks_range  
+
+    def modify_grade(self, new_grade, new_marks_range):
+        self.grade = new_grade
+        self.marks_range = new_marks_range
+
+    def delete_grade(self):
+        del self.grade
+        print("Grade deleted successfully")
+
+
 class LoginUser:
     def __init__(self, email_id, password):
         self.email_id = email_id
@@ -95,3 +108,14 @@ class LoginUser:
         self.password = new_password
         print("Password changed successfully")
 
+    def  Logout():
+        print("Logged out successfully")
+        return True 
+
+    def Encrypt_password():
+        # Implement encryption logic here
+        pass    
+
+    def Decrypt_password():
+    # Implement decryption logic here
+        pass    
